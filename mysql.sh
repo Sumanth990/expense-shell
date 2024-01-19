9b528a56ad0b7254effc0791dcbe6e02d42a483a
Sumanth <9ssumanth@gmail.com>
2024-01-19 16:45:43 +0530

    mysql password test

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,13 @@
-${MySQL_PASSWORD}=$1
+#${MySQL_PASSWORD}=$1
 
 dnf module disable mysql -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass ${MySQL_PASSWORD}
+mysql_secure_installation --set-root-pass $1
+
+#${MySQL_PASSWORD}
 
 
 # password: ExpenseApp@1
