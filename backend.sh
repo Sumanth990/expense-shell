0d01ebb1f877425bb8e6072f059f3c39ca8d337e
Sumanth <9ssumanth@gmail.com>
2024-01-21 17:42:43 +0530

    Adding success or failure status instead of $?

--- backend.sh
+++ backend.sh
@@ -6,24 +6,49 @@ source common.sh
 
 Head "disable default version of nodeJS"
 dnf module disable nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 Head "enable nodeJS18 version"
 dnf module enable nodejs:18 -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 Head "install nodeJS"
 dnf install nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 Head "adding application user"
 useradd expense &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 
 Head "configure backend service"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 #we are keeping "/app" here because conf file will fail due to change of directory path.
 
@@ -31,21 +56,41 @@ app_prereq "/app"
 
 Head "Installing application dependencies"
 npm install &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 Head "reloading systemd and start backend service"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 Head "Install MySQL client"
 dnf install mysql -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 Head "Load schema"
 mysql -h mysql-dev.aquireawsdevops.online -uroot -p${MySQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
 #We can use $1 instead of ${MySQL_PASSWORD}
 
--- common.sh
+++ common.sh
@@ -9,19 +9,39 @@ app_prereq () {
 
   Head "remove existing app content"
   rm -rf $1 &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
   Head "create application directory"
   mkdir $1 &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
   Head "Download application content"
   curl -o /tmp/${component}.zip https://expense-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 
   cd $1
 
   Head "Extracting application content"
   unzip /tmp/${component}.zip &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+echo Success
+else
+echo Failure
+exit 1
+fi
 }
